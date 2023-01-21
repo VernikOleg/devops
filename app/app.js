125e1341a2184e38c1a96ae91ee0c2d24542379b
olegvernik <donbassporozhnyaknyegonit@gmail.com>
2023-01-21 13:06:47 +0200

    changed

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("<h4> <center> 333-333-333 </center> </h4> <br> <h4> <center> We Present </center> </h4> <br> <h3> <center> A SPECIAL </center> </h3> <br> <h3> <center> PRESENTATION </center> </h3>")
+  res.send("<h4> <center> 333-333-333 </center> </h4> <br> <h4> <center> We Present </center> </h4> <br> <h3> A SPECIAL </h3> <br> <h3> PRESENTATION </h3>")
 })
 
 app.listen(port, () => {
